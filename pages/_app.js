8e44934c2d50a1edd3e78993a33de5f66ad54c7e
Valery Nazemkin <vnazemkin@fil-it.ru>
2020-10-11 13:59:23 +0300

    Finished

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
+import '../styles/main.scss';
+import NextNprogress from 'nextjs-progressbar';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
+export default function MyApp({ Component, pageProps }) {
+    return (
+        <>
+            <NextNprogress
+            color="#29D"
+            startPosition="0.3"
+            stopDelayMs="200"
+            height="3"
+            />
+            <Component {...pageProps} />
+        </>
+    );
+  }
